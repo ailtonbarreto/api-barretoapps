cd2ffe1e7cb7c870b0ef158b3c3259ce1f5bd961
Ailton Barreto <143955093+ailtonbarreto@users.noreply.github.com>
2025-04-01 20:33:12 -0300

    Update db.js

--- db.js
+++ db.js
@@ -88,6 +88,51 @@ app.get("/localizacoes", async (req, res) => {
   }
 });
 
+// --------------------------------------------------------------------------------------
+// INSERIR AGENDAMENTO
+
+app.post("/input_agendamento", async (req, res) => {
+
+  const { nome, data, hora_inicio, hora_fim, profissional } = req.body;
+
+
+  try {
+
+    const query = "INSERT INTO u771906953_barreto.agenda (nome, data, hora_inicio, hora_fim, profissional) VALUES (?, ?, ?, ?, ?)";
+    
+    pool.query(query, [nome, data, hora_inicio, hora_fim, profissional], (err, results) => {
+      if (err) {
+        return res.status(500).json({ error: "Erro ao salvar no banco de dados", details: err });
+      }
+      res.status(200).json({ message: "Localização e foto recebidas com sucesso!", data: results });
+    });
+
+  } catch (err) {
+    console.error("Erro ao salvar", err);
+    res.status(500).json({ error: "Erro ao processar", details: err.message });
+  }
+});
+
+// --------------------------------------------------------------------------------------
+// VER AGENDAMENTO
+
+app.get("/agendamento", async (req, res) => {
+  try {
+    const query = "SELECT * FROM u771906953_barreto.agenda";
+    
+    pool.query(query, (err, results) => {
+      if (err) {
+        return res.status(500).json({ error: "Erro ao buscar dados no banco de dados", details: err });
+      }
+
+      res.status(200).json({ data: formattedResults });
+    });
+
+  } catch (err) {
+    console.error("Erro ao consultar as agenda:", err);
+    res.status(500).json({ error: "Erro ao consultar as agenda", details: err.message });
+  }
+});
 
 
 // --------------------------------------------------------------------------------------
